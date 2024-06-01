f4075dbd6b8107f219d00209801c7560a3e4d0a9
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2024-06-01 08:19:06 -0500

    FinishTsconfigUpdate

---
+++ auto-import.d.ts
@@ -0,0 +1,12 @@
+export declare const imports: {
+    '@solidjs/router': string[];
+    '~/di': string[];
+    '~/env': string[];
+    'fp-ts/lib/function': string[];
+    'vitest-mock-extended': string[];
+}[];
+export declare const importTypes: {
+    from: string;
+    imports: string[];
+    type: boolean;
+}[];
